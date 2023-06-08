893a5226f1b895f0f95eb46f82ea38f061c05393
Hyuk Jin Choi <62084630+sc62070@users.noreply.github.com>
2023-06-08 16:47:40 +0900

    #1 menu still working on it

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MenuManager Instance;
+
+    [SerializeField] Menu[] menus;
+
+    void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenMenu(string menuName)
     {
-        
+        for(int i = 0; i < menus.Length; i++)
+        {
+            if(menus[i].menuName == menuName)
+            {
+                OpenMenu(menus[i]);
+            }
+            else if(menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
     }
+
+    public void OpenMenu(Menu menu)
+    {
+        for(int i = 0; i < menus.Length; i++)
+        {
+             if(menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+        menu.Open();
+    }
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
+    }
+
 }
